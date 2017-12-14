442ea4f3c475a05cbe69e6a8496c2c7b17a9aadb
Eugene Shangin <fessracha@gmail.com>
2017-12-14 12:07:44 +0500

    afishaView new position div

--- views/afisha/index.php
+++ views/afisha/index.php
@@ -1,24 +1,26 @@
 <?php include ROOT . '/views/template/header/header.php'; ?>
 
 <div class="spacing"></div>
-
+<div class="container">
 <?php foreach($afishaList as $afishaItem): ?>
-<div class="row">
-    <div class="ml-auto col-md-1 afishaItemDate text-center">
+<div class="row afishaItem">
+
+    <div class="ml-auto col-md-2 afishaItemDate text-center">
         <h1><?php echo $afishaItem['day'] . '/' . $afishaItem['month'];?></h1><br>
         <h4><?php echo $afishaItem['dweek']; ?></h4>
     </div>
-    <div class="col-md-3 afishaItemInfo text-center">
+    <div class="col-md-4 afishaItemInfo text-center">
         <h4><?php echo $afishaItem['spectacleName']; ?></h4><br>
         <p><?php echo $afishaItem['adress']; ?></p><br>
         <p><?php echo $afishaItem['time']; ?></p>
     </div>
-    <div class="col-md-1 afishaItemButton mr-auto text-center">
+    <div class="col-md-2 afishaItemButton mr-auto text-center">
     <a href="/spectacle/view<?php echo $afishaItem['spectacleId'];?>">ПОДРОБНЕЕ</a>
     </div>
 </div>
-<div class="ml-auto col-md-6 mr-auto spacingBorder"></div>
+<div class="ml-auto col-md-6 mr-auto spacing"></div>
 <?php endforeach; ?>
+</div><!--container-->
 
 <div class="spacing"></div>
 <div class="spacing"></div>
